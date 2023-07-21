f515cf4d87efb76dee039fe745d94453c6f23762
Vithursa <vithu30@hotmail.com>
2023-07-21 23:13:43 +0800

    setup

---
+++ commonutils/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='commonutils',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies required by your package here
+    ],
+)
