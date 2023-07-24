b9cac9c6992c4970eb707aeacb1052998d3b11df
Vithursa <vithu30@hotmail.com>
2023-07-24 10:06:20 +0800

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -3,5 +3,6 @@ zip -r lib.zip commonutils
 export test="$(dirname "$(pwd)")/lib.zip"
 echo $test
  export PYTHONPATH=$PYTHONPATH:$test
+ echo $PYTHONPATH
  pytest ./api/tests.py
  pytest ./api/tests.py --doctest-modules --junitxml=junit/test-results.xml --cov=com --cov-report=xml --cov-report=html
